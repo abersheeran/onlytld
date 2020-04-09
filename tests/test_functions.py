a0b90d341aa66ba7c953c36663a2bd9ff223b8dd
abersheeran <me@abersheeran.com>
2020-04-09 19:39:21 +0800

    first commit

---
+++ tests/test_functions.py
@@ -0,0 +1,25 @@
+import pytest
+
+from onlytld import get_tld
+
+
+@pytest.mark.parametrize(
+    "domain, tld",
+    [
+        ("chinese.cn", "cn"),
+        ("hk", "hk"),
+        ("abersheeran.com", "com"),
+        ("google.cn", "cn"),
+        ("www.lijinlong.cc", "cc"),
+        ("a.com.cn", "com.cn"),
+        ("ahnu.edu.cn", "edu.cn"),
+        ("www.ck", "ck"),
+        ("www.moanasands.co.ck", "co.ck"),
+        ("co.ck", "co.ck"),
+        ("adf.yokohama.jp", "adf.yokohama.jp"),
+        ("city.yokohama.jp", "yokohama.jp"),
+        ("aber.dfsdf", None),
+    ],
+)
+def test_get_tld(domain: str, tld: str) -> None:
+    assert get_tld(domain) == tld
